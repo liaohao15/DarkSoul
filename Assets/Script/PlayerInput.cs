348d0aae27dd0a4001194850c2b349f29baacdec
liaohao15 <3526250586@qq.com>
2025-11-17 20:13:59 +0800

    修正，添加开关InputEnable

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -26,6 +26,8 @@ public class Player : MonoBehaviour
     public float DupVelocity;
     public float DturnVelocity;
 
+    public bool InputEnable = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,7 +46,15 @@ public class Player : MonoBehaviour
         TargetDup = ((Input.GetKey(KeyCode.W)?1.0f:0) - (Input.GetKey(KeyCode.S) ? 1.0f : 0));
         TargetDturn = ((Input.GetKey(KeyCode.A) ? 1.0f : 0) - (Input.GetKey(KeyCode.D) ? 1.0f : 0));
 
+        if (InputEnable == false)//使用InputEnable开关来控制玩家的输入功能
+        {
+            TargetDturn = 0;
+            TargetDup = 0;
+        
+        }
+
+
         Dup = Mathf.SmoothDamp(Dup, TargetDup, ref DupVelocity, 0.1f);
-        Dturn = Mathf.SmoothDamp(Dturn, TargetDturn, ref DturnVelocity, 0.1f);
+        Dturn = Mathf.SmoothDamp(Dturn, TargetDturn, ref DturnVelocity, 0.1f);//平滑输入是为了，更好的与动作动画搭配
     }
 }
