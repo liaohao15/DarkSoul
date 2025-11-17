db238b74c7e127f1a9eb5a25f08c7ec9067d44e4
liaohao15 <3526250586@qq.com>
2025-11-17 19:59:30 +0800

    第三次提交，实现输入平滑

--- Assets/Script/PlayerInput.cs
+++ Assets/Script/PlayerInput.cs
@@ -13,14 +13,18 @@ public class Player : MonoBehaviour
     //这种写法是将按键变量名存为字符串，但是Input.GetKey()的参数，不能识别这类，得用KeyCode类型
     public KeyCode KeyUp = KeyCode.W;
     public KeyCode KeyDown = KeyCode.S;
-    public KeyCode KeyLift = KeyCode.A;
+    public KeyCode KeyLeft = KeyCode.A;
     public KeyCode KeyRight = KeyCode.D;
     //用正负值来决定上下左右键，其实就是将输入键转化为数值Image
+
     public float Dup;
     public float Dturn;
-    
-    
-    
+
+    public float TargetDup;
+    public float TargetDturn;
+
+    public float DupVelocity;
+    public float DturnVelocity;
 
 
     // Start is called before the first frame update
@@ -29,12 +33,18 @@ public class Player : MonoBehaviour
         
     }
 
+    private float GetDupVelocity()
+    {
+        return DupVelocity;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Dup = ((Input.GetKey(KeyCode.W)?1.0f:0) - (Input.GetKey(KeyCode.S) ? 1.0f : 0));
-        Dturn = ((Input.GetKey(KeyCode.A) ? 1.0f : 0) - (Input.GetKey(KeyCode.D) ? 1.0f : 0));
+        TargetDup = ((Input.GetKey(KeyCode.W)?1.0f:0) - (Input.GetKey(KeyCode.S) ? 1.0f : 0));
+        TargetDturn = ((Input.GetKey(KeyCode.A) ? 1.0f : 0) - (Input.GetKey(KeyCode.D) ? 1.0f : 0));
 
-
+        Dup = Mathf.SmoothDamp(Dup, TargetDup, ref DupVelocity, 0.1f);
+        Dturn = Mathf.SmoothDamp(Dturn, TargetDturn, ref DturnVelocity, 0.1f);
     }
 }
